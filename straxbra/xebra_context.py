0266c720da5d55b0bbddd49d263ff094aeaea813
Darryl Masson <darryl.masson@physik.uni-freiburg.de>
2019-05-27 14:39:45 +0200

    Accepts runlists from website

--- straxbra/xebra_context.py
+++ straxbra/xebra_context.py
@@ -19,6 +19,8 @@ class XebraContext(strax.Context):
         super().__init__(*args, **kwargs)
 
     def get_array(self, run_id, *args, **kwargs) -> np.ndarray:
+        if isinstance(run_id, str) and len(run_id) > 5:  # runlist from website
+            run_id = [f'{int(run_id[i:i+4],16)}' for i in range(0, len(run_id), 4)]
         if isinstance(run_id, (str, int)):
             return super().get_array(run_id, *args, **kwargs)
         return np.concatenate([super().get_array(run,
@@ -27,6 +29,8 @@ class XebraContext(strax.Context):
                                for run in tqdm.tqdm(run_id)])
 
     def get_df(self, run_id, *args, **kwargs) -> pd.DataFrame:
+        if isinstance(run_id, str) and len(run_id) > 5:  # runlist from website
+            run_id = [f'{int(run_id[i:i+4],16)}' for i in range(0, len(run_id), 4)]
         if isinstance(run_id, (str, int)):
             return super().get_df(run_id, *args, **kwargs)
         return pd.DataFrame().append([super().get_df(run,
